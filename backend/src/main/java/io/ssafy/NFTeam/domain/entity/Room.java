b5f2a6cf7321e21768cf6ad8a0ee5bba76de5e77
WoowonKim <dyffh1031@gmail.com>
2022-09-13 01:41:30 +0900

    refactor: room entity @NotNull 어노테이션 추가, id -> roomId 변경

--- backend/src/main/java/io/ssafy/NFTeam/domain/entity/Room.java
+++ backend/src/main/java/io/ssafy/NFTeam/domain/entity/Room.java
@@ -1,21 +1,26 @@
 package io.ssafy.NFTeam.domain.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Getter
-@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Room {
 
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roomId;
+    @NotNull
     private String title;
+    @NotNull
     private String createdAt;
+    @NotNull
     private String host;
 }
