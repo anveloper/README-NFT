d24eda1e2dc3eb275a8d9b73a74c3acd599d74eb
WoowonKim <dyffh1031@gmail.com>
2022-09-13 01:40:43 +0900

    feat: room service 계층 작성

--- backend/src/main/java/io/ssafy/NFTeam/api/service/RoomService.java
+++ backend/src/main/java/io/ssafy/NFTeam/api/service/RoomService.java
@@ -1,39 +1,19 @@
-//package io.ssafy.NFTeam.api.service;
-//
-//import io.ssafy.NFTeam.domain.entity.Room;
-//import io.ssafy.NFTeam.domain.repository.RoomRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.List;
-//
-//@Service
-//@Transactional(readOnly = true)
-//@RequiredArgsConstructor
-//public class RoomService {
-//
-//    private final RoomRepository roomRepository;
-//
-//    //room 생성
-//    @Transactional
-//    public Room createRoom(Room room){
-//        return roomRepository.createRoom(room);
-//    }
-//
-//    //전체 room 조회
-//    public List<Room> getRooms(){
-//        return roomRepository.findAll();
-//    }
-//
-//    //room id 조횐
-//    public Room getRoomById(Long roomId){
-//        return roomRepository.findRoom(roomId);
-//    }
-//
-//    //room 삭제
-//    @Transactional
-//    public void deleteRoom(Long roomId) {
-//        roomRepository.deleteRoom(roomId);
-//    }
-// }
+package io.ssafy.NFTeam.api.service;
+
+import io.ssafy.NFTeam.api.request.RoomPostReq;
+import io.ssafy.NFTeam.api.response.RoomGetRes;
+
+import java.util.List;
+
+public interface RoomService {
+    public RoomGetRes createRoom(RoomPostReq requestDto);
+
+    //전체 room 조회
+    public List<RoomGetRes> getRooms();
+
+    //room id 조횐
+    public RoomGetRes getRoomById(Long roomId);
+
+    //room 삭제
+    public void deleteRoom(Long roomId);
+}
