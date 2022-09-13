b99b4fc47985d9adb94875cb86c507158bb76dbf
jhkang9820 <jhkang9820@gmail.com>
2022-09-14 00:41:51 +0900

    design: NFT 디테일 컴포넌트 배치

--- frontend/src/features/detail/Detail.tsx
+++ frontend/src/features/detail/Detail.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import styled from "styled-components";
 
 const Detail = () => {
-  return <div>HI!</div>;
+  return (
+    <div className="detail">
+      <div className="detail-container">
+        <div className="cards">
+          <div className="card-img">카드 이미지</div>
+          <div className="card-contents">카드 내용</div>
+        </div>
+        <div className="cards">
+          <div className="card-img">카드 이미지</div>
+          <div className="card-contents">카드 내용</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
