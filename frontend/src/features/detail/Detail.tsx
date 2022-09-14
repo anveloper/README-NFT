00341e713432b10f8c07d8af1e4adaa89346c493
jhkang9820 <jhkang9820@gmail.com>
2022-09-14 17:57:08 +0900

    design: NFT 디테일 module.css 분리

--- frontend/src/features/detail/Detail.tsx
+++ frontend/src/features/detail/Detail.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import styled from "styled-components";
+import styles from "./Detail.module.css";
 
 const Detail = () => {
   return (
-    <div className="detail">
-      <div className="detail-container">
-        <div className="cards">
-          <div className="card-img">카드 이미지</div>
-          <div className="card-contents">카드 내용</div>
+    <div className={styles.detail}>
+      <div className={styles.detail_container}>
+        <div className={styles.cards}>
+          <div className={styles.card_img}>카드 이미지</div>
+          <div className={styles.card_contents}>카드 내용</div>
         </div>
-        <div className="cards">
-          <div className="card-img">카드 이미지</div>
-          <div className="card-contents">카드 내용</div>
+        <div className={styles.cards}>
+          <div className={styles.card_img}>카드 이미지</div>
+          <div className={styles.card_contents}>카드 내용</div>
         </div>
       </div>
     </div>
