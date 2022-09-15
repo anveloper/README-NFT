c41d3446bea6eaeb60065a7998d3948f0adce9b9
anveloper <hitedin@gmail.com>
2022-09-15 19:52:40 +0900

    feat: 메인페이지 캐러셀 구현

--- frontend/src/features/main/Carousel.tsx
+++ frontend/src/features/main/Carousel.tsx
@@ -4,11 +4,13 @@ import { ReactComponent as Cloud1 } from "../../assets/carousel/carousel_cloud01
 import { ReactComponent as Cloud2 } from "../../assets/carousel/carousel_cloud02.svg";
 import { ReactComponent as Plane1 } from "../../assets/carousel/plane01.svg";
 import { ReactComponent as Plane2 } from "../../assets/carousel/plane02.svg";
+import CarouselSwiper from "./components/CarouselSwiper";
 
 import styles from "./Main.module.css";
 const Carousel = () => {
   return (
     <div className={styles.carouselContainer}>
+      <CarouselSwiper />
       <Cloud1 className={styles.rightCloud} />
       <Cloud2 className={styles.leftCloud} />
       <Plane1 className={styles.leftPlane} />
