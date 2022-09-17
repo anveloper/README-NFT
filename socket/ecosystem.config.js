646fb95ca9eddfcdcfcbe9778e61ce5d8e2d32c1
siwon-park <zow777@naver.com>
2022-09-18 01:35:28 +0900

    docs: ecosystem.config.js 파일 수정

--- socket/ecosystem.config.js
+++ socket/ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = [{
-  script: 'dist/server.js',
-  name: 'NFTeam_Socket_Server',
-  exec_mode: 'cluster',
-  instances: 2,
+  script: 'babel-node dist/server.js',
+  name: 'npm',
+  // exec_mode: 'cluster',
+  // instances: 2,
   env_production: {
     NODE_ENV: "production"
   },
