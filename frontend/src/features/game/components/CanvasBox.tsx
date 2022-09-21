89e97777ea682a05dca27090e3f0d1228376672b
anveloper <hitedin@gmail.com>
2022-09-22 00:58:59 +0900

    feat: 캔버스 초기화 로직 변경

--- frontend/src/features/game/components/CanvasBox.tsx
+++ frontend/src/features/game/components/CanvasBox.tsx
@@ -1,16 +1,21 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../../app/hooks";
 import {
+  selectAnswer,
   selectColor,
   selectHostUserName,
   selectSocket,
+  selectSolver,
   selectStarted,
   selectTimeover,
+  setSolver,
   setTimeover,
 } from "../gameSlice";
 // css
 import styles from "../Game.module.css";
 import { Modal } from "../../../components/modal/Modal";
+import { setRawData, setTmpInfo } from "../../mint/mintSlice";
+import { useNavigate } from "react-router-dom";
 
 export interface Coordinate {
   x: number;
@@ -21,6 +26,8 @@ const CanvasBox = () => {
   const socket = useAppSelector(selectSocket);
   const hostUserName = useAppSelector(selectHostUserName);
   const color = useAppSelector(selectColor);
+  const answer = useAppSelector(selectAnswer);
+  const solver = useAppSelector(selectSolver);
   const timeover = useAppSelector(selectTimeover);
   const started = useAppSelector(selectStarted);
 
@@ -30,6 +37,7 @@ const CanvasBox = () => {
   const [canvasWidth, setCanvasWidth] = useState(0);
   const [canvasHeight, setCanvasHeight] = useState(0);
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (boxRef.current) {
@@ -147,8 +155,10 @@ const CanvasBox = () => {
         const canvas: HTMLCanvasElement = canvasRef.current;
         const ctx = canvas.getContext("2d");
         if (ctx) {
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
           ctx.fillStyle = "white";
-          ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+          ctx.fillRect(0, 0, canvas.width, canvas.height);
+          console.log("reset_draw");
         }
       });
       socket.on("set_data", (data: string) => {
@@ -157,7 +167,7 @@ const CanvasBox = () => {
         const ctx = canvas.getContext("2d");
         if (ctx) {
           ctx.fillStyle = "white";
-          ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+          ctx.fillRect(0, 0, canvas.width, canvas.height);
         }
         const init = JSON.parse(data);
         for (let i = 0; i < init.length; i++) {
@@ -165,20 +175,22 @@ const CanvasBox = () => {
           drawLine({ x: x0, y: y0 }, { x: x1, y: y1 }, color);
         }
       });
+      socket.on("send_solver", (solver) => {
+        dispatch(setSolver(solver));
+      });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [socket]);
 
   // =============== file
-  const handleMint = () => {};
-
   const [fileName, setFileName] = useState("");
   const [fileUrl, setFileUrl] = useState("");
-
+  const [newRawData, setNewRawData] = useState("");
   useEffect(() => {
     if (canvasRef.current) {
       const canvas: HTMLCanvasElement = canvasRef.current;
       const imgBase64 = canvas.toDataURL("image/jpeg", "image/octet-stream");
+      setNewRawData(imgBase64);
       const decodImg = atob(imgBase64.split(",")[1]);
       let array = [];
       for (let i = 0; i < decodImg.length; i++) {
@@ -191,9 +203,25 @@ const CanvasBox = () => {
         `${hostUserName}` + new Date().getMilliseconds() + ".png";
       setFileName(fileName);
       setFileUrl(window.URL.createObjectURL(file));
+      if (socket) socket.emit("get_solver", hostUserName);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [hostUserName, timeover]);
   // =============== file end
+
+  const handleMint = () => {
+    dispatch(setRawData(newRawData));
+    const tmpUrl = fileUrl;
+
+    if (socket) {
+      socket.emit("game_end", hostUserName, (ans: string, sol: string) => {
+        dispatch(setTmpInfo({ answer, creator: hostUserName, solver, tmpUrl }));
+        console.log(ans, sol);
+        navigate("/mint");
+      });
+    }
+  };
+
   return (
     <>
       <Modal
@@ -219,10 +247,16 @@ const CanvasBox = () => {
             다운로드
           </a>
         </h6>
+        <div>정답: {answer}</div>
+        <div>만든이: {hostUserName}</div>
+        <div>맞춘이: {solver}</div>
+        <div>임시 URL: {fileUrl}</div>
         <br />
         게임을 종료하시겠습니까?
         <br />
-        취소를 누르면 추가시간을 부여할 수 있습니다.
+        취소를 누르면 추가시간을 부여하거나 다시 그릴 수 있습니다.
+        <br />
+        확인을 누르면 세션이 종료되고, 민팅화면으로 전환됩니다.
       </Modal>
       <div ref={boxRef} className={styles.canvasBox}>
         <canvas
