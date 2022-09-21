20221dae592fb044e1a43db83f85e5f0388901c2
hyeon <lhynjn9@gmail.com>
2022-09-21 13:04:59 +0900

    feat: 민팅 컨트랙트 수정

---
+++ smart-contracts/contracts/SaleReadmeToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/interfaces/IERC20.sol";
+import "../node_modules/@openzeppelin/contracts/interfaces/IERC721.sol";
+import "./ReadmeToken.sol";
+
+contract SaleReadmeToken{
+    
+}
