75c18a06aabeb254f8b882b79dcfc88b371a955d
anveloper <hitedin@gmail.com>
2022-09-22 14:06:08 +0900

    feat: 참여자 목록 출력 기능 추가

--- frontend/src/features/game/Game.tsx
+++ frontend/src/features/game/Game.tsx
@@ -9,8 +9,6 @@ import {
   selectRoomCnt,
   selectRoomName,
   selectSocket,
-  selectStarted,
-  setColor,
 } from "./gameSlice";
 import { selectUserAddress } from "../auth/authSlice";
 // component
@@ -22,13 +20,14 @@ import ChatBox from "./components/ChatBox";
 import CanvasSpring from "../../assets/live-item/canvas_spring.svg";
 // css
 import styles from "./Game.module.css";
+import ToolBox from "./components/ToolBox";
+import MemberBox from "./components/MemberBox";
 const Game = () => {
   const userAddress = useAppSelector(selectUserAddress);
   const socket = useAppSelector(selectSocket);
   const roomName = useAppSelector(selectRoomName);
   const hostUserName = useAppSelector(selectHostUserName);
   const roomCnt = useAppSelector(selectRoomCnt);
-  const started = useAppSelector(selectStarted);
 
   const [tabFlag, setTabFlag] = useState(true);
   const dispatch = useAppDispatch();
@@ -44,26 +43,21 @@ const Game = () => {
 
   const handleExit = () => {
     if (socket) {
-      socket.emit("leave_room", hostUserName);
       if (hostUserName === userAddress) {
-        socket.emit("game_end", hostUserName);
+        socket.emit(
+          "game_end",
+          hostUserName,
+          (answer: string, solver: string) => {
+            console.log(answer, solver);
+          }
+        );
       }
+      socket.emit("leave_room", hostUserName);
     }
     dispatch(resetRoomInfo());
     navigate("/live");
   };
 
-  const handleColor = (e: any) => {
-    e.preventDefault();
-    dispatch(setColor(e.target.value));
-  };
-  const handleCanvasReset = () => {
-    if (socket) {
-      if (userAddress !== hostUserName) return;
-      socket.emit("reset_canvas", hostUserName);
-      console.log(userAddress, hostUserName);
-    }
-  };
   return (
     <div className={styles.container}>
       <NewHelmet
@@ -83,35 +77,7 @@ const Game = () => {
             <img className={styles.springTwo} src={CanvasSpring} alt="" />
             <CanvasBox />
           </div>
-          <div className={styles.toolBox}>
-            <form className={styles.colorBox} onClick={handleColor}>
-              <button value={"#000000"} style={{ background: "#000000" }}>
-                검정
-              </button>
-              <button value={"#D93D04"} style={{ background: "#D93D04" }}>
-                빨강
-              </button>
-              <button value={"#FDDF61"} style={{ background: "#FDDF61" }}>
-                노랑
-              </button>
-              <button value={"#3B82BF"} style={{ background: "#3B82BF" }}>
-                파랑
-              </button>
-              <button value={"#79C4F2"} style={{ background: "#79C4F2" }}>
-                하늘
-              </button>
-            </form>
-            <div className={styles.erase}>
-              <button
-                onClick={() => {
-                  dispatch(setColor("#ffffff"));
-                }}
-              >
-                지우개
-              </button>
-              <button onClick={handleCanvasReset}>초기화</button>
-            </div>
-          </div>
+          <ToolBox />
         </div>
         <TimerBox />
       </div>
@@ -131,7 +97,8 @@ const Game = () => {
             참여자({roomCnt})
           </button>
         </div>
-        <ChatBox />
+        <MemberBox visible={!tabFlag} />
+        <ChatBox/>
       </div>
     </div>
   );
