abb6bdb6f17fc52df16ade3a2226bdb582ce0005
김주연 <jyeon3930@naver.com>
2022-09-24 00:37:05 +0900

    refactor: API URI 정리

---
+++ frontend/src/api/api.ts
@@ -0,0 +1,44 @@
+// 기본 url
+const HOST = process.env.REACT_APP_API_BASE_URL + "/api/";
+
+// 세부 url
+const LIKE = "like";
+const ROOM = "room";
+const SOLVER = "solver";
+
+interface apiInterface {
+  like: {
+    likeNFT: () => string;
+    likeNFTList: (walletAddress: string) => string;
+  };
+  room: {
+    getAllRoomList: () => string;
+    makeRoom: () => string;
+    getRoom: (id: any) => string;
+    deleteRoom: (id: any) => string;
+  };
+  solver: {
+    solveProblem: () => string;
+    getSolveList: (walletAddress: any, nftAddress: any) => string;
+  };
+}
+
+const api: apiInterface = {
+  like: {
+    likeNFT: () => HOST + LIKE,
+    likeNFTList: (walletAddress) => HOST + LIKE + "/" + walletAddress,
+  },
+  room: {
+    getAllRoomList: () => HOST + ROOM,
+    makeRoom: () => HOST + ROOM,
+    getRoom: (id) => HOST + ROOM + "/" + id,
+    deleteRoom: (id) => HOST + ROOM + "/" + id,
+  },
+  solver: {
+    solveProblem: () => HOST + SOLVER,
+    getSolveList: (walletAddress, nftAddress) =>
+      HOST + SOLVER + "/" + walletAddress + "/" + nftAddress,
+  },
+};
+
+export default api;
