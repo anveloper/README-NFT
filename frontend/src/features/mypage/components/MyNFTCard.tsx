9e1efeb94c6e7ea11bce491c3008a613cc738293
김주연 <jyeon3930@naver.com>
2022-09-26 17:16:01 +0900

    fix: 마이페이지 NFT 리스트 출력 수정

--- frontend/src/features/mypage/components/MyNFTCard.tsx
+++ frontend/src/features/mypage/components/MyNFTCard.tsx
@@ -1,5 +1,6 @@
 // image
-import { FC } from "react";
+import axios from "axios";
+import { FC, useEffect, useState } from "react";
 import lion from "../../../assets/characters/lion.svg";
 // css
 import styles from "../MyPage.module.css";
@@ -12,18 +13,43 @@ export interface IMyNFTCard {
 }
 
 const MyNFTCard: FC<IMyNFTCard> = ({ tokenId, price, owner, metadataURI }) => {
+  const [fileName, setFileName] = useState("");
+  const [name, setName] = useState("");
+  const [author, setAuthor] = useState("");
+  const [description, setDescription] = useState("");
+  const [imageURL, setImageURL] = useState("");
+
+  const getNFTDetail = async (metadataURI: string) => {
+    try {
+      await axios({ url: metadataURI }).then((response) => {
+        const { fileName, name, author, description, imageURL } = response.data;
+        setFileName(fileName);
+        setName(name);
+        setAuthor(author);
+        setDescription(description);
+        setImageURL(imageURL);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getNFTDetail(metadataURI);
+  }, [metadataURI]);
+
   return (
     <div className={styles.MyNFTCard}>
       <img className={styles.MyNFTCardImg} src={lion} alt="" />
 
       <div>
         <div className={styles.MyNFTCardTextBox}>
-          <h4 className={styles.MyNFTCardLeftText}>NFT Name</h4>
-          <p className={styles.MyNFTCardRightText}>Creater</p>
+          <h4 className={styles.MyNFTCardLeftText}>{name}</h4>
+          <p className={styles.MyNFTCardRightText}>{author}</p>
         </div>
 
         <div className={styles.MyNFTCardTextBox}>
-          <h5 className={styles.MyNFTCardLeftText}>22 SSF</h5>
+          <h5 className={styles.MyNFTCardLeftText}>{price} SSF</h5>
           <p className={styles.MyNFTCardRightText}>First Solver</p>
         </div>
       </div>
