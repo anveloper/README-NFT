2314b20edf7a66afc1c161daa50e6c6e13923887
jhkang9820 <jhkang9820@gmail.com>
2022-09-28 00:20:58 +0900

    design: 메인 NFT 리스트 모달 연결 및 CSS 일부 수정

--- frontend/src/components/nftItem/NftItem.tsx
+++ frontend/src/components/nftItem/NftItem.tsx
@@ -3,7 +3,8 @@ import axios from "axios";
 
 import styles from "./NftItem.module.css";
 import { truncatedAddress } from "../../features/auth/authSlice";
-import { useNavigate } from "react-router-dom";
+import ModalPortal from "../modal/ModalPortal";
+import NftDetailModal from "../../features/detail/NftDetailModal";
 
 const NftItem = (props: any) => {
   const { nft } = props;
@@ -12,7 +13,8 @@ const NftItem = (props: any) => {
   const [author, setAuthor] = useState("");
   const [description, setDescription] = useState("");
   const [imageURL, setImageURL] = useState("");
-  const navigate = useNavigate();
+
+  const [modalOpen, setModalOpen] = useState(false);
 
   const getMetadata = async (metaDataURI: string) => {
     try {
@@ -29,10 +31,13 @@ const NftItem = (props: any) => {
     }
   };
 
+  const openModal = () => {
+    setModalOpen(true);
+  };
 
-  const moveToDetail = (tokenId: string) => {
-    navigate('/detail/'+ tokenId);
-  }
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   useEffect(() => {
     const { metaDataURI } = props;
@@ -40,25 +45,33 @@ const NftItem = (props: any) => {
   }, [props]);
 
   return (
-    <button className={styles.container} onClick={()=>moveToDetail(nft.readmeTokenId)}>
-      <div className={styles.card}>
-        <div className={styles.front}>
-          <Suspense fallback={<p>이미지 로딩중</p>}>
-            <img className={styles.img} src={imageURL} alt="" />
-          </Suspense>
+    // {/* <button className={styles.container} onClick={() => moveToDetail(nft.readmeTokenId)}> */}
+    <div className={styles.container}>
+      <div onClick={openModal}>
+        <div className={styles.card}>
+          <div className={styles.front}>
+            <Suspense fallback={<p>이미지 로딩중</p>}>
+              <img className={styles.img} src={imageURL} alt="" />
+            </Suspense>
+          </div>
+          <div className={styles.back}>
+            <p>리드미: {name}</p>
+            <p>작성자: {truncatedAddress(author)}</p>
+            <p>맞춘이: {truncatedAddress(description)}</p>
+            <small>파일이름: {fileName}</small>
+          </div>
         </div>
-        <div className={styles.back}>
-          <p>리드미: {name}</p>
-          <p>작성자: {truncatedAddress(author)}</p>
-          <p>맞춘이: {truncatedAddress(description)}</p>
-          <small>파일이름: {fileName}</small>
+        <div className={styles.nftInfo}>
+          <p>리드미ID: {nft.readmeTokenId}번째</p>
+          <p>PRICE: {nft.readmeTokenPrice}</p>
         </div>
       </div>
-      <div className={styles.nftInfo}>
-        <p>리드미ID: {nft.readmeTokenId}번째</p>
-        <p>PRICE: {nft.readmeTokenPrice}</p>
-      </div>
-    </button>
+      {modalOpen && (
+        <ModalPortal>
+          <NftDetailModal open={modalOpen} close={closeModal} image={imageURL} answer={name} tokenId={nft.readmeTokenId} />
+        </ModalPortal>
+      )}
+    </div>
   );
 };
 
