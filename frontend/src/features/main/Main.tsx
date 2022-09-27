48df7c2f8a14ac180052f4ac807d28fe1ead26f8
anveloper <hitedin@gmail.com>
2022-09-27 22:22:06 +0900

    feat: 메인페이지 스크롤 액션 구현완료

--- frontend/src/features/main/Main.tsx
+++ frontend/src/features/main/Main.tsx
@@ -1,10 +1,22 @@
 // core
-import React, { useState, useEffect, useRef } from "react";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+  useMemo,
+} from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { io } from "socket.io-client";
 // state
-import { selectSocket, setRoomInfo, setSocket } from "../game/gameSlice";
+import {
+  selectSocket,
+  setRoomInfo,
+  setSocket,
+  setView,
+  sview,
+} from "../game/gameSlice";
 import { selectUserAddress, selectUserName } from "../auth/authSlice";
 import RoomButton from "./components/RoomButton";
 // component
@@ -15,6 +27,7 @@ import { Modal } from "../../components/modal/Modal";
 // css
 import styles from "./Main.module.css";
 import Guide from "./Guide";
+import { throttle } from "lodash";
 
 const socketURL =
   process.env.NODE_ENV !== "production"
@@ -25,6 +38,13 @@ const Main = () => {
   const socket = useAppSelector(selectSocket);
   const userAddress = useAppSelector(selectUserAddress);
   const userName = useAppSelector(selectUserName);
+
+  const mainRef = useRef<HTMLDivElement | null>(null);
+  const guideRef = useRef<HTMLDivElement | null>(null);
+  const carouselRef = useRef<HTMLDivElement | null>(null);
+  const tabRef = useRef<HTMLDivElement | null>(null);
+  const contentRef = useRef<HTMLDivElement | null>(null);
+  const view = useAppSelector(sview);
 
   const [modalOpen, setModalOpen] = useState(false);
   const [registerRoomName, setRegisterRoomName] = useState("");
@@ -41,6 +61,57 @@ const Main = () => {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [socket]);
+
+  const throttleWheel = useMemo(
+    () =>
+      throttle((value: number) => {
+        // console.log(value);
+        if (value > 0 && view < 6) dispatch(setView(view + 1));
+        else if (value > 0 && view === 6) dispatch(setView(5));
+        else if (value < 0 && view >= 0) dispatch(setView(view - 1));
+      }, 300),
+    [dispatch, view]
+  );
+
+  const wheelAction = useCallback(
+    (e: WheelEvent) => {
+      const delY = e.deltaY;
+      if (Math.abs(delY) > 200) e.preventDefault();
+      throttleWheel(delY);
+    },
+    [throttleWheel]
+  );
+
+  useEffect(() => {
+    if (view <= 0)
+      guideRef.current.scrollIntoView({
+        block: "start",
+        behavior: "smooth",
+      });
+    else if (view === 1 || view === 2)
+      carouselRef.current.scrollIntoView({
+        block: "start",
+        behavior: "smooth",
+      });
+    else if (view === 3 || view === 4)
+      tabRef.current.scrollIntoView({
+        block: "start",
+        behavior: "smooth",
+      });
+    else
+      mainRef.current.scrollIntoView({
+        block: "end",
+        behavior: "smooth",
+      });
+  }, [view]);
+
+  useEffect(() => {
+    mainRef?.current.addEventListener("wheel", wheelAction);
+    return () => {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      mainRef?.current?.removeEventListener("wheel", wheelAction);
+    };
+  });
 
   const closeModal = () => {
     setModalOpen(false);
@@ -71,15 +142,16 @@ const Main = () => {
     }
   };
   return (
-    <div>
+    <div ref={mainRef}>
       <NewHelmet
         title="리드미 & NFT"
         description="README 게임 라이브 목록 및 NFT 목록을 보여줍니다."
       />
-      <Guide />
-      <Carousel />
-      <MainTab />
+      <Guide guideRef={guideRef} />
+      <Carousel carouselRef={carouselRef} />
+      <MainTab tabRef={tabRef} />
       <Outlet />
+      <div ref={contentRef} />
       <Modal
         open={modalOpen}
         close={closeModal}
@@ -103,7 +175,7 @@ const Main = () => {
           />
         </div>
       </Modal>
-      {socket && <RoomButton setModalOpen={setModalOpen} />}
+      {socket && socket.connected && <RoomButton setModalOpen={setModalOpen} />}
     </div>
   );
 };
