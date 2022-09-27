a4d5257f6c0f0325fd685f271299879cdf4635b7
jhkang9820 <jhkang9820@gmail.com>
2022-09-28 00:18:51 +0900

    feat: NFT 디테일 모달창 생성 및 CSS 작업

---
+++ frontend/src/components/modal/ModalPortal.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+interface ModalPortalProps {
+  children: React.ReactNode;
+}
+
+const ModalPortal = ({ children }: ModalPortalProps) => {
+  const modalRoot = document.getElementById("modal") as HTMLElement;
+
+  return ReactDOM.createPortal(children, modalRoot);
+};
+
+export default ModalPortal;
