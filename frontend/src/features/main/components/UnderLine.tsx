9075c43f3dc407aa436089c6a3ae2fe3710f24f6
anveloper <hitedin@gmail.com>
2022-09-27 22:22:47 +0900

    design: 주소값 검사하여 디자인 상태값 변경

--- frontend/src/features/main/components/UnderLine.tsx
+++ frontend/src/features/main/components/UnderLine.tsx
@@ -1,21 +1,24 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import styles from "../Main.module.css";
 interface Under {
   under: string;
 }
 const UnderLine = ({ under }: Under) => {
+  const { pathname } = useLocation();
+  const un = pathname !== "/list" ? "left" : under;
   return (
     <div className={styles.underBox}>
       <p
         style={{
-          width: under === "left" ? "0px" : "40vw",
+          width: un === "left" ? "0px" : "40vw",
           transition: "all 0.3s",
         }}
       />
       <p className={styles.underLine} />
       <p
         style={{
-          width: under === "right" ? "0px" : "40vw",
+          width: un === "right" ? "0px" : "40vw",
           transition: "all 0.3s",
         }}
       />
