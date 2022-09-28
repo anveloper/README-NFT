8f633b387a54a3cdb3e3d0bfe98a4d23a19d02be
jhkang9820 <jhkang9820@gmail.com>
2022-09-28 11:32:15 +0900

    design: 메인 NFT 리스트 CSS 수정

--- frontend/src/components/nftItem/NftItem.tsx
+++ frontend/src/components/nftItem/NftItem.tsx
@@ -44,33 +44,45 @@ const NftItem = (props: any) => {
 
   return (
     // {/* <button className={styles.container} onClick={() => moveToDetail(nft.readmeTokenId)}> */}
-    <div className={styles.container}>
-      <div onClick={openModal}>
-        <div className={styles.card}>
-          <div className={styles.front}>
-            <Suspense fallback={<p>이미지 로딩중</p>}>
-              <img className={styles.img} src={imageURL} alt="" />
-            </Suspense>
-          </div>
-          <div className={styles.back}>
-            <p>리드미: {name}</p>
-            <p>작성자: {truncatedAddress(author)}</p>
-            <p>맞춘이: {truncatedAddress(description)}</p>
-            <small>파일이름: {fileName}</small>
+      <>
+      <div className={styles.container} onClick={openModal}>
+          <div className={styles.card}>
+            <div className={styles.front}>
+              <Suspense fallback={<p>이미지 로딩중</p>}>
+                <img className={styles.img} src={imageURL} alt="" />
+              </Suspense>
+              <div className={styles.nftInfo}>
+                <p>리드미ID: {nft.readmeTokenId}번째</p>
+                <p>PRICE: {nft.readmeTokenPrice}</p>
+              </div>
+            </div>
+            {/* <div className={styles.back}>
+              <p>리드미: {name}</p>
+              <p>작성자: {truncatedAddress(author)}</p>
+              <p>맞춘이: {truncatedAddress(description)}</p>
+              <small>파일이름: {fileName}</small>
+            </div> */}
           </div>
         </div>
-        <div className={styles.nftInfo}>
-          <p>리드미ID: {nft.readmeTokenId}번째</p>
-          <p>PRICE: {nft.readmeTokenPrice}</p>
-        </div>
+        <div>
+        {modalOpen && (
+          <ModalPortal>
+            <NftDetailModal open={modalOpen} close={closeModal} image={imageURL} answer={name} tokenId={nft.readmeTokenId} />
+          </ModalPortal>
+        )}
       </div>
-      {modalOpen && (
-        <ModalPortal>
-          <NftDetailModal open={modalOpen} close={closeModal} image={imageURL} answer={name} tokenId={nft.readmeTokenId} />
-        </ModalPortal>
-      )}
-    </div>
+        </>
   );
 };
 
 export default NftItem;
+
+/*
+<div>
+        {modalOpen && (
+          <ModalPortal>
+            <NftDetailModal open={modalOpen} close={closeModal} image={imageURL} answer={name} tokenId={nft.readmeTokenId} />
+          </ModalPortal>
+        )}
+      </div>
+*/
