3f365864a890c54a9e83b972aa811dea1d5650c5
jhkang9820 <jhkang9820@gmail.com>
2022-10-06 00:27:36 +0900

    design: NFT 구매 확인 모달 CSS

--- frontend/src/features/detail/NftDetailInfo.tsx
+++ frontend/src/features/detail/NftDetailInfo.tsx
@@ -8,17 +8,16 @@ import { useEffect, useContext, useState } from "react";
 import { SocketContext } from "socketConfig";
 
 interface nftTime {
-  year: number,
-  month: number,
-  day: number,
-  hour: number,
-  minute: number,
-  second: number
+  year: number;
+  month: number;
+  day: number;
+  hour: number;
+  minute: number;
+  second: number;
 }
 
 const NftDetailInfo = (props: any) => {
-  const { isActive, nftOwner, userAddress, tokenId, nftDetail, nftPrice } =
-    props;
+  const { isActive, nftOwner, userAddress, tokenId, nftDetail, nftPrice } = props;
   const [modalOpen, setModalOpen] = useState(false);
   const [nftEndTime, setNftEndTime] = useState<nftTime>({
     year: 0,
@@ -37,7 +36,7 @@ const NftDetailInfo = (props: any) => {
     const response = await SaleReadmeContract.methods.readmeTokenEndTime(tokenId).call();
     // console.log("결과", response);
     await SaleReadmeContract.methods.parseTimestamp(response).call((err: any, res: any) => {
-      const nftEndTime : nftTime = {
+      const nftEndTime: nftTime = {
         year: Number(res.year),
         month: Number(res.month),
         day: Number(res.day),
@@ -111,7 +110,7 @@ const NftDetailInfo = (props: any) => {
     navigate(-1);
   };
 
-  useEffect( () => {
+  useEffect(() => {
     getTimeInfo();
   }, []);
 
@@ -125,7 +124,9 @@ const NftDetailInfo = (props: any) => {
                 <>
                   <div>판매 중입니다.</div>
                   <div>즉시 구매하시거나, 경매에 참여할 수 있습니다.</div>
-                  <div>종료일: {nftEndTime.year}년 {nftEndTime.month}월 {nftEndTime.day}일 {nftEndTime.hour}시 {nftEndTime.minute}분 {nftEndTime.second}초 </div>
+                  <div>
+                    종료일: {nftEndTime.year}년 {nftEndTime.month}월 {nftEndTime.day}일 {nftEndTime.hour}시 {nftEndTime.minute}분 {nftEndTime.second}초{" "}
+                  </div>
                 </>
               ) : (
                 // <>
@@ -156,19 +157,13 @@ const NftDetailInfo = (props: any) => {
                   <>
                     {isActive ? (
                       <>
-                        <button
-                          className={styles.card_button}
-                          onClick={cancelSale}
-                        >
+                        <button className={styles.card_button} onClick={cancelSale}>
                           판매 취소
                         </button>
                       </>
                     ) : (
                       <>
-                        <button
-                          className={styles.card_button}
-                          onClick={() => props.setTab("sell")}
-                        >
+                        <button className={styles.card_button} onClick={() => props.setTab("sell")}>
                           즉시 판매
                         </button>
                       </>
@@ -176,11 +171,7 @@ const NftDetailInfo = (props: any) => {
                   </>
                 ) : (
                   <>
-                    <button
-                      disabled={!isActive}
-                      className={styles.card_button}
-                      onClick={openModal}
-                    >
+                    <button disabled={!isActive} className={styles.card_button} onClick={openModal}>
                       즉시 구매
                     </button>
                   </>
@@ -190,19 +181,30 @@ const NftDetailInfo = (props: any) => {
           </div>
         </div>
       </div>
-      <Modal
-        className={styles.modal_container}
-        open={modalOpen}
-        close={closeModal}
-        fn={buyNftToken}
-        header="리드미 구매 확인"
-      >
-        
-        <div className={styles.modal_img_container}>
+      <Modal open={modalOpen} close={closeModal} fn={buyNftToken} header="리드미 구매 확인">
+        <div className={styles.modal_container}>
           <img className={styles.modal_img} src={nftDetail.imageURL} alt="" />
+          <div className={styles.modal_info_container}>
+            <div className={styles.modal_info}>
+              <div className={styles.modal_info_text1}>
+                <div>제목</div>
+                <div>{nftDetail.name}</div>
+              </div>
+              <div className={styles.modal_info_text1}>
+                <div>가격</div>
+                <div>{nftPrice} SSF</div>
+              </div>
+            </div>
+            <div className={styles.modal_info}>
+              <div className={styles.modal_info_text2}>
+                <div>구매하시겠습니까?</div>
+              </div>
+              <div className={styles.modal_info_text2}>
+                <div>확인 버튼을 누르시면 구매가 완료됩니다.</div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div>이거 진짜 살거?</div>
-        <div>{nftPrice} SSF : 이 가격에 ???</div>
       </Modal>
     </>
   );
