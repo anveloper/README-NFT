93db2c497c035333617e21092ca880823fbbdb8a
김주연 <jyeon3930@naver.com>
2022-10-06 11:16:58 +0900

    design: SSAFY 토큰 추가 가이드 페이지 재디자인

--- frontend/src/routes/NetGuide.tsx
+++ frontend/src/routes/NetGuide.tsx
@@ -1,11 +1,13 @@
 import { useNavigate } from "react-router-dom";
+// css
 import styles from "./NetGuide.module.css";
-import cloud1 from "assets/welcome/page3_cloud1.svg";
-import cloud2 from "assets/welcome/page3_cloud2.svg";
-import g_1 from "assets/guide-img/g_1.jpg";
-import g_2 from "assets/guide-img/g_2.jpg";
-import g_3 from "assets/guide-img/g_3.jpg";
-import g_4 from "assets/guide-img/g_4.jpg";
+// img
+import guidebook from "assets/guide/guidebook.svg";
+import guide1 from "assets/guide/guide1.svg";
+import guide2 from "assets/guide/guide2.svg";
+import guide3 from "assets/guide/guide3.svg";
+import guide4 from "assets/guide/guide4.svg";
+import guide5 from "assets/guide/guide5.svg";
 
 import Slider from "react-slick";
 import { useState } from "react";
@@ -27,19 +29,76 @@ const NetGuide = () => {
     "Readme에서는 SSF토큰을 사용합니다! 웰컴 페이지에서 SSF토큰을 지갑에 불러와 주세요",
   ];
   return (
-    <div className={styles.welcomePageThree}>
-      <h1 className={styles.WelcomeTitleText}>싸피네트워크 추가하기</h1>
-      <img className={styles.page3_cloud1} src={cloud1} alt="" />
-      <img className={styles.page3_cloud2} src={cloud2} alt="" />
+    <div className={styles.NetGuide}>
+      <img className={styles.guidebook} src={guidebook} alt="" />
       <Slider className={styles.slider} {...setting}>
-        <img src={g_1} alt="" />
-        <img src={g_2} alt="" />
-        <img src={g_3} alt="" />
-        <img src={g_4} alt="" />
+        <div className={styles.sliderDiv}>
+          <div className={styles.sliderImgDiv}>
+            <img className={styles.guide1} src={guide1} alt="" />
+          </div>
+          <div className={styles.sliderTextDiv}>
+            <h4 className={styles.sliderText}>
+              ① Chrome 오른쪽 상단 확장 프로그램을 열어주세요. <br /> <br /> ②
+              Metamask를 실행해 주세요.
+            </h4>
+          </div>
+        </div>
+
+        <div className={styles.sliderDiv}>
+          <div className={styles.sliderImgDiv}>
+            <img className={styles.guide2} src={guide2} alt="" />
+          </div>
+          <div className={styles.sliderTextDiv}>
+            <h4 className={styles.sliderText}>
+              Metamask 팝업 상단 네트워크를 클릭해주세요.
+            </h4>
+          </div>
+        </div>
+
+        <div className={styles.sliderDiv}>
+          <div className={styles.sliderImgDiv}>
+            <img className={styles.guide3} src={guide3} alt="" />
+          </div>
+          <div className={styles.sliderTextDiv}>
+            <h4 className={styles.sliderText}>
+              네트워크 목록 하단 'Add network' 버튼을 클릭해주세요.
+            </h4>
+          </div>
+        </div>
+
+        <div className={styles.sliderDiv}>
+          <div className={styles.sliderImgDiv}>
+            <img className={styles.guide4} src={guide4} alt="" />
+          </div>
+          <div className={styles.sliderTextDiv}>
+            <h4 className={styles.sliderText}>
+              아래 내용을 입력 후 저장해주세요. <br /> <br /> <br />
+              <br />
+              Network name &nbsp; : &nbsp; SSAFY Network
+              <br /> <br />
+              New RPC URL &nbsp; : &nbsp; http://20.196.209.2:8545 <br />
+              <br />
+              Chain ID &nbsp; :&nbsp; 31221 <br />
+              <br />
+              Currency symbol&nbsp; :&nbsp; ETH
+              <br />
+            </h4>
+          </div>
+        </div>
+
+        <div className={styles.sliderDiv}>
+          <div className={styles.sliderImgDiv}>
+            <img className={styles.guide5} src={guide5} alt="" />
+          </div>
+          <div className={styles.sliderTextDiv}>
+            <h4 className={styles.sliderText}>
+              리드미에서는 SSAFY 토큰을 사용합니다! <br /> <br />
+              웰컴 페이지에서 'SSF 추가하기'를 클릭하면 등록이 완료됩니다!
+            </h4>
+          </div>
+        </div>
       </Slider>
-      <div className={styles.textBox}>
-        <h4 className={styles.WelcomeDescriptionText}>{guides[activeSlide]}</h4>
-      </div>
+      {/* <div className={styles.textBox}></div> */}
       {/* <button
         onClick={() => {
           navigate(`/`);
